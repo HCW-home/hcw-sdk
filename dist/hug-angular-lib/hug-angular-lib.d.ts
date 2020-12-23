6765a312176d9cb2ad7955925bfb379bbfc7bee2
Muhammed <aapozaid@gmail.com>
2020-12-24 00:01:28 +0200

    include dist

--- dist/hug-angular-lib/hug-angular-lib.d.ts
+++ dist/hug-angular-lib/hug-angular-lib.d.ts
@@ -2,5 +2,3 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public-api';
-
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaHVnLWFuZ3VsYXItbGliLmQudHMiLCJzb3VyY2VzIjpbImh1Zy1hbmd1bGFyLWxpYi5kLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7QUFDQTtBQUNBIiwic291cmNlc0NvbnRlbnQiOlsiLyoqXG4gKiBHZW5lcmF0ZWQgYnVuZGxlIGluZGV4LiBEbyBub3QgZWRpdC5cbiAqL1xuZXhwb3J0ICogZnJvbSAnLi9wdWJsaWMtYXBpJztcbiJdfQ==
