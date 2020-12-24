589a9407e40b17564e2c362ecf74f3e2c7231e34
Muhammed <aapozaid@gmail.com>
2020-12-24 22:18:16 +0200

    fix logger

--- projects/hug-angular-lib/src/lib/hug-angular-lib.module.ts
+++ projects/hug-angular-lib/src/lib/hug-angular-lib.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { HugAngularLibComponent } from './hug-angular-lib.component';
 import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 
-
+console.log('NGX logger')
 @NgModule({
   declarations: [HugAngularLibComponent],
   imports: [
-    LoggerModule.forRoot({serverLoggingUrl: '/api/logs', level: NgxLoggerLevel.DEBUG, serverLogLevel: NgxLoggerLevel.ERROR})
+    // LoggerModule.forRoot({serverLoggingUrl: '/api/logs', level: NgxLoggerLevel.DEBUG, serverLogLevel: NgxLoggerLevel.ERROR})
   ],
   exports: [HugAngularLibComponent]
 })
